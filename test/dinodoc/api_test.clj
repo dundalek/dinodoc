abe4c088621022476c2945c0090dc92121db06cd
Jakub Dundalek <dundalek@gmail.com>
2023-10-23 19:09:19 +0200

    tests for generate

--- test/dinodoc/api_test.clj
+++ test/dinodoc/api_test.clj
@@ -35,6 +35,52 @@
                            :outdir outdir
                            :github/repo "repo"
                            :git/branch "main"})
-        (is (true? (-> (fsdata outdir)
-                       (get-in ["api" "example" "main" "index.md"])
-                       (str/includes? "\n### foo {#foo}\n"))))))))
+        (is (str/includes?
+             (get-in (fsdata outdir) ["api" "example" "main" "index.md"])
+             "\n### foo {#foo}\n"))))))
+
+(deftest generate-api-mode-global
+  (with-temp-dir
+    (fn [{:keys [dir fspit]}]
+      (fspit "a/src/example/a_main.clj" "(ns example.a-main)\n(defn foo [])")
+      (fspit "b/src/example/b_main.clj" "(ns example.b-main)\n(defn bar [])")
+      (let [outdir (str dir "/docs-separate")
+            _ (dinodoc/generate {:paths [(str dir "/a")
+                                         (str dir "/b")]
+                                 :outdir outdir
+                                 :github/repo "repo"
+                                 :git/branch "main"})
+            data (fsdata outdir)]
+        (is (str/includes? (get-in data ["a" "api" "example" "a-main" "index.md"])
+                           "\n### foo {#foo}\n"))
+        (is (str/includes? (get-in data ["b" "api" "example" "b-main" "index.md"])
+                           "\n### bar {#bar}\n")))
+      (let [outdir (str dir "/docs-global")
+            _ (dinodoc/generate {:paths [(str dir "/a")
+                                         (str dir "/b")]
+                                 :outdir outdir
+                                 :api-docs :global
+                                 :github/repo "repo"
+                                 :git/branch "main"})
+            data (fsdata outdir)]
+        (is (str/includes? (get-in data ["api" "example" "a-main" "index.md"])
+                           "\n### foo {#foo}\n"))
+        (is (str/includes? (get-in data ["api" "example" "b-main" "index.md"])
+                           "\n### bar {#bar}\n"))))))
+
+(deftest generate-doc-tree
+  (with-temp-dir
+    (fn [{:keys [dir fspit]}]
+      (fspit "README.md" "# This is readme")
+      (fspit "doc/a.md" "# Page A")
+      (fspit "doc/b.md" "# Page B")
+      (let [outdir (str dir "/docs")]
+        (dinodoc/generate {:paths [{:path dir
+                                    :outdir "."}]
+                           :outdir outdir
+                           :github/repo "repo"
+                           :git/branch "main"})
+        (is (= {"a.md" "---\n{sidebar_position: 1, custom_edit_url: repo/tree/main/doc/a.md}\n---\n\n# Page A",
+                "b.md" "---\n{sidebar_position: 2, custom_edit_url: repo/tree/main/doc/b.md}\n---\n\n# Page B",
+                "index.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/README.md}\n---\n\n# This is readme"}
+               (fsdata outdir)))))))
