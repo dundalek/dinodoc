f9fde25119bc4327b0b7004865df924dd561d878
Jakub Dundalek <dundalek@gmail.com>
2023-10-24 12:05:46 +0200

    more doc-tree tests

--- test/dinodoc/api_test.clj
+++ test/dinodoc/api_test.clj
@@ -39,6 +39,19 @@
              (get-in (fsdata outdir) ["api" "example" "main" "index.md"])
              "\n### foo {#foo}\n"))))))
 
+(deftest generate-blank
+  (with-temp-dir
+    (fn [{:keys [dir fspit]}]
+      (let [outdir (str dir "/docs")]
+        (fspit "src/example/main.clj" "(ns example.main)\n")
+        (dinodoc/generate {:paths [{:path dir
+                                    :outdir "."}]
+                           :outdir outdir
+                           :github/repo "repo"
+                           :git/branch "main"})
+        (testing "No file is created if there are no vars"
+          (is (= {} (fsdata outdir))))))))
+
 (deftest generate-api-mode-global
   (with-temp-dir
     (fn [{:keys [dir fspit]}]
@@ -113,3 +126,72 @@
                                :github/repo "repo"
                                :git/branch "main"})
             (is (= expected (fsdata outdir)))))))))
+
+(deftest generate-doc-tree-with-nesting
+  (with-temp-dir
+    (fn [{:keys [dir fspit]}]
+      (fspit "doc/a.md" "# Page A")
+      (fspit "doc/b.md" "# Page B")
+      (fspit "doc/c.md" "# Page C")
+      (fspit "doc/d.md" "# Page D")
+      (testing "nested"
+        (let [outdir (str dir "/docs-nested")
+              doc-tree [["Top-level B" {:file "doc/b.md"}
+                         ["The A" {:file "doc/a.md"}]
+                         ["The C" {:file "doc/c.md"}]]]]
+          (dinodoc/generate {:paths [{:path dir
+                                      :outdir "."
+                                      :doc-tree doc-tree}]
+                             :outdir outdir
+                             :github/repo "repo"
+                             :git/branch "main"})
+          (is (= {"d.md" "---\n{sidebar_position: 1, custom_edit_url: repo/tree/main/doc/d.md}\n---\n\n# Page D"
+                  "index.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/b.md, sidebar_label: Top-level B}\n---\n\n# Page B"
+                  "top-level-b" {"the-a.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/a.md, sidebar_label: The A}\n---\n\n# Page A"
+                                 "the-c.md" "---\n{sidebar_position: 1, custom_edit_url: repo/tree/main/doc/c.md, sidebar_label: The C}\n---\n\n# Page C"}}
+                 (fsdata outdir)))))
+      (testing "more-nesting"
+        (let [outdir (str dir "/docs-more-nesting")
+              doc-tree [["Top-level B" {:file "doc/b.md"}
+                         ["The A" {:file "doc/a.md"}
+                          ["The C" {:file "doc/c.md"}]]]]]
+          (dinodoc/generate {:paths [{:path dir
+                                      :outdir "."
+                                      :doc-tree doc-tree}]
+                             :outdir outdir
+                             :github/repo "repo"
+                             :git/branch "main"})
+          (is (= {"d.md" "---\n{sidebar_position: 1, custom_edit_url: repo/tree/main/doc/d.md}\n---\n\n# Page D"
+                  "index.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/b.md, sidebar_label: Top-level B}\n---\n\n# Page B"
+                  "top-level-b" {"the-a" {"index.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/a.md, sidebar_label: The A}\n---\n\n# Page A"
+                                          "the-c.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/c.md, sidebar_label: The C}\n---\n\n# Page C"}}}
+                 (fsdata outdir))))))))
+
+(deftest generate-doc-tree-categories-without-content
+  (with-temp-dir
+    (fn [{:keys [dir fspit]}]
+      (fspit "doc/a.md" "# Page A")
+      (fspit "doc/b.md" "# Page B")
+      (fspit "doc/c.md" "# Page C")
+      (fspit "doc/d.md" "# Page D")
+      (let [doc-tree [["Top-level B" {:file "doc/b.md"}
+                       ["A Category" {}
+                        ["The A" {:file "doc/a.md"}]]
+                       ["C Category" {}
+                        ["The C" {:file "doc/c.md"}]]
+                       ["The D" {:file "doc/d.md"}]]]]
+        (testing "nested"
+          (let [outdir (str dir "/docs")]
+            (dinodoc/generate {:paths [{:path dir
+                                        :outdir "."
+                                        :doc-tree doc-tree}]
+                               :outdir outdir
+                               :github/repo "repo"
+                               :git/branch "main"})
+            (is (= {"index.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/b.md, sidebar_label: Top-level B}\n---\n\n# Page B"
+                    "top-level-b" {"a-category" {"_category_.json" "{\"position\":0,\"label\":\"A Category\"}"
+                                                 "the-a.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/a.md, sidebar_label: The A}\n---\n\n# Page A"}
+                                   "c-category" {"_category_.json" "{\"position\":1,\"label\":\"C Category\"}"
+                                                 "the-c.md" "---\n{sidebar_position: 0, custom_edit_url: repo/tree/main/doc/c.md, sidebar_label: The C}\n---\n\n# Page C"}
+                                   "the-d.md" "---\n{sidebar_position: 2, custom_edit_url: repo/tree/main/doc/d.md, sidebar_label: The D}\n---\n\n# Page D"}}
+                   (fsdata outdir)))))))))
