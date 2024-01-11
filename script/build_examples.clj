25111151d4c8c826403c99f17987bebc5ed040f7
Jakub Dundalek <dundalek@gmail.com>
2024-01-11 16:12:26 +0100

    tasks

--- script/build_examples.clj
+++ script/build_examples.clj
@@ -11,14 +11,20 @@
   (fs/delete-tree target-path)
   (fs/copy-tree (str source-dir "/docs") target-path))
 
-(defn -main [& _args]
+(defn build-dinodoc-docs []
   (let [target-path "website/docs/docs"
         source-dir "."]
-    (build-and-copy-docs source-dir target-path))
+    (build-and-copy-docs source-dir target-path)))
+
+(defn build-examples-docs []
   (doseq [example ["polylith" "promesa" "reitit" "ring"]]
     (let [target-path (str "website/docs/examples/" example)
           source-dir (str "examples/" example)]
       (build-and-copy-docs source-dir target-path))))
+
+(defn -main [& _args]
+  (build-dinodoc-docs)
+  (build-examples-docs))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
