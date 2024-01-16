abb8482b1306690bfcb4569aec2fe481b59f8a69
Jakub Dundalek <dundalek@gmail.com>
2024-01-16 10:08:35 +0100

    fixup to relativize readme links

--- doc.clj
+++ doc.clj
@@ -1,5 +1,7 @@
 (ns doc
   (:require
+   [babashka.fs :as fs]
+   [clojure.string :as str]
    [dinodoc.api :as dinodoc]))
 
 (dinodoc/generate
@@ -7,5 +9,12 @@
   :output-path "docs"
   :git/branch "main"
   :github/repo "https://github.com/dundalek/dinodoc"})
+
+;; Fixup to change absolute URLs to relative links in readme.
+;; Having absolute URLs in README.md to make them work when opened on Github.
+;; For Docusaurus relative links are better for local preview and for better
+;; experience because external links would get opened in a new browser tab.
+(fs/update-file "docs/index.md"
+                str/replace #"https://dinodoc.pages.dev" "")
 
 (shutdown-agents)
