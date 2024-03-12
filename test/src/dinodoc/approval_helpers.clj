c4c4cfcd68e271dbb71ef3c556f1d754daab7046
Jakub Dundalek <dundalek@gmail.com>
2024-03-12 17:20:50 +0100

    improve diff reporting for approval tests

--- test/src/dinodoc/approval_helpers.clj
+++ test/src/dinodoc/approval_helpers.clj
@@ -1,10 +1,18 @@
 (ns dinodoc.approval-helpers
   (:require
    [clojure.java.shell :refer [sh]]
-   [clojure.test :refer [is]]))
+   [clojure.test :refer [is]]
+   [clojure.string :as str]))
 
 ;; We compare if output is the same as committed state, git status returns empty output if there are no changes.
 ;; If implementation changes and expected state needs to be updated, commit the changes after inspecting the diff.
-(defmacro is-same? [path]
-  `(is (= {:exit 0 :err "" :out ""}
-          (sh "git" "status" "--porcelain" ~path))))
+
+#_(defmacro is-same? [path]
+    `(is (= {:exit 0 :err "" :out ""}
+            (sh "git" "status" "--porcelain" ~path))))
+
+(defn is-same? [path]
+  (let [result (sh "git" "status" "--porcelain" path)]
+    (is (= 0 (:exit result)))
+    (is (= "" (:err result)))
+    (is (= [""] (str/split-lines (:out result))))))
