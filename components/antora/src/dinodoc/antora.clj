f9b68975b5d4e93a871c652a2653558968f11676
Jakub Dundalek <dundalek@gmail.com>
2024-04-10 15:15:45 +0200

    antora: initial markdown conversion helper

--- components/antora/src/dinodoc/antora.clj
+++ components/antora/src/dinodoc/antora.clj
@@ -1,6 +1,7 @@
 (ns dinodoc.antora
   (:require
    [babashka.fs :as fs]
+   [clojure.java.shell :as shell]
    [clojure.string :as str]))
 
 (defn- generate-navigation-items [path base-path level]
@@ -21,3 +22,13 @@
   (let [base-path (str/replace (str path) #"/?$" "/")]
     (->> (generate-navigation-items path base-path "*")
          (str/join "\n"))))
+
+(defn md->adoc [input]
+  (-> (shell/sh "pandoc" "--from" "markdown" "--to" "asciidoc-auto_identifiers"
+                "--shift-heading-level-by=-1"
+                "--standalone"
+                :in input)
+      :out
+      ;; Post-processing to use the `[#id]` syntax instead of legacy `[[id]]`,
+      ;; otherwise asciidoctor would not parse ids starting with dash `-`.
+      (str/replace #"(?m)^\[\[(.*)\]\]$" "[#$1]")))
--- components/antora/test/src/dinodoc/antora_test.clj
+++ components/antora/test/src/dinodoc/antora_test.clj
@@ -30,3 +30,15 @@
       (fspit "c.adoc" "")
       (is (= "* xref:a.adoc[]\n* b\n** xref:b/b1.adoc[]\n** xref:b/b2.adoc[]\n* xref:c.adoc[]"
              (antora/generate-navigation dir))))))
+
+(deftest md->adoc-test
+  (is (= "= h1\n\n" (antora/md->adoc "# h1\n\n")))
+  (is (= "link:example.com[Example]\n" (antora/md->adoc "[Example](example.com)\n")))
+
+  ;; it would be better if the output did not includ heading id `[#h2]` when not explicitly defined
+  (is (= "= h1\n\n[#h2]\n== h2\n" (antora/md->adoc "# h1\n\n## h2\n")))
+  (is (= "[#h2]\n== h2\n" (antora/md->adoc "## h2\n"))))
+
+(deftest md->adoc-test-heading-ids
+  (is (= "[#greet]\n== greet\n" (antora/md->adoc "## greet {#greet}")))
+  (is (= "[#-main]\n== -main\n" (antora/md->adoc "## -main {#-main}"))))
