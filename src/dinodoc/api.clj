235bf9bf27c803784ffd6859bbea0f6da6cb6b1c
Jakub Dundalek <dundalek@gmail.com>
2024-04-23 16:29:32 +0200

    WIP

--- src/dinodoc/api.clj
+++ src/dinodoc/api.clj
@@ -30,21 +30,22 @@ Options:
                     (map #(impl/normalize-input % root-opts)))
         global-analysis (when (= api-mode :global)
                           (impl/run-analysis (mapcat :source-paths inputs)))
-        inputs (->> inputs
-                    (map (fn [input]
-                           (if (:generator input)
-                             (assoc input
-                                    :generator-output-path (:output-path input)
-                                    :generator-output-prefix (:output-path input))
-                             (assoc input
-                                    :generator-output-path (:api-docs-dir input)
-                                    :generator-output-prefix (:api-docs-prefix input)
-                                    :cljapi-generator true
-                                    :generator
-                                    (cljapi/make-generator
-                                     (-> (select-keys input [:source-paths :path :github/repo :git/branch])
-                                         (assoc :global-analysis global-analysis))))))))
-        resolve-from-generators (impl/make-resolve-link inputs)]
+        article-generator-inputs (->> inputs
+                                      (remove :generator))
+        generator-inputs (->> inputs
+                              (map (fn [input]
+                                     (if (:generator input)
+                                       (assoc input
+                                              :generator-output-path (:output-path input)
+                                              :generator-output-prefix (:output-path input))
+                                       (assoc input
+                                              :generator-output-path (:api-docs-dir input)
+                                              :generator-output-prefix (:api-docs-prefix input)
+                                              :generator
+                                              (cljapi/make-generator
+                                               (-> (select-keys input [:source-paths :path :github/repo :git/branch])
+                                                   (assoc :global-analysis global-analysis))))))))
+        resolve-from-generators (impl/make-resolve-link generator-inputs)]
 
     (fs/delete-tree root-outdir)
     (fs/create-dirs root-outdir)
@@ -64,32 +65,33 @@ Options:
           (spit (str api-docs-dir "/_category_.json")
                 "{\"label\":\"API\"}"))))
 
-    (doseq [{:keys [generator]} inputs]
+    (doseq [{:keys [generator]} generator-inputs]
       (generator/prepare-index generator))
 
-    (doseq [input inputs]
-      (when (:cljapi-generator input)
-        (let [{:keys [path doc-tree output-path path-to-root-fn edit-url-fn]} input
-              doc-tree-opts {:root-path output-path
-                             :parent-path output-path
-                             :input-path path
-                             :edit-url-fn edit-url-fn}
-              doc-tree-ops (impl/process-doc-tree-pure doc-tree-opts doc-tree)
-              file-map (->> doc-tree-ops
-                            (filter #(= (first %) :copy-with-frontmatter))
-                            (map (fn [[_ {:keys [file target]}]]
-                                   [file (str/replace-first target (str output-path "/") "")]))
-                            (into {}))
-              resolve-apilink (or resolve-apilink resolve-from-generators)
-              link-resolver (fn [file-path s]
-                              (when-some [target (resolve-apilink s)]
+    (doseq [input article-generator-inputs]
+      (let [{:keys [path doc-tree output-path path-to-root-fn edit-url-fn]} input
+            doc-tree-opts {:root-path output-path
+                           :parent-path output-path
+                           :input-path path
+                           :edit-url-fn edit-url-fn}
+            doc-tree-ops (impl/process-doc-tree-pure doc-tree-opts doc-tree)
+            file-map (->> doc-tree-ops
+                          (filter #(= (first %) :copy-with-frontmatter))
+                          (map (fn [[_ {:keys [file target]}]]
+                                 [file (str/replace-first target (str output-path "/") "")]))
+                          (into {}))
+            resolve-apilink (or resolve-apilink resolve-from-generators)
+            link-resolver (fn [file-path s]
+                            (when-some [target (resolve-apilink s)]
                                 ;; pathname:// workaround for non-absolute links to HTML assets
                                 ;; https://github.com/facebook/docusaurus/issues/3894#issuecomment-740622170
-                                (let [html-target? (re-find #"\.html$|\.html#.*$" target)]
-                                  (str (when html-target? "pathname://") (path-to-root-fn file-path) "/" target))))]
+                              (let [html-target? (re-find #"\.html$|\.html#.*$" target)]
+                                (str (when html-target? "pathname://") (path-to-root-fn file-path) "/" target))))]
 
-          (impl/process-doc-tree! doc-tree-ops {:file-map file-map
-                                                :link-resolver link-resolver})))
+        (impl/process-doc-tree! doc-tree-ops {:file-map file-map
+                                              :link-resolver link-resolver})))
+
+    (doseq [input generator-inputs]
       (when (not= api-mode :global)
         (let [{:keys [generator generator-output-path]} input]
           (generator/generate generator {:output-path generator-output-path}))))))
